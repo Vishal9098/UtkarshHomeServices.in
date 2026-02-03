40703e2195c04939f4659b7333b18dfeb4d36ac6
Vishal9098 <vishalsilawat6@gmail.com>
2026-02-03 18:34:21 +0530

    i

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,13 +1,9 @@
 from .models import Cart
 
 def cart_count(request):
+    count = 0
     if request.session.session_key:
         count = Cart.objects.filter(
             session_key=request.session.session_key
         ).count()
-    else:
-        count = 0
-
-    return {
-        'totalitem': count
-    }
+    return {'cart_count': count}
